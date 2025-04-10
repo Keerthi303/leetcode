1e4478aa599752d471b634fc4e080e3b06475a59
keerthi <g.keerthiraghava37@gmail.com>
2025-04-10 22:01:50 +0530

    Time: 0 ms (100%), Space: 13.9 MB (26.61%) - LeetHub

--- 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.c
+++ 2836-neither-minimum-nor-maximum/2836-neither-minimum-nor-maximum.c
@@ -1,14 +1,14 @@
 int findNonMinOrMax(int* nums, int numsSize) {
     if(numsSize==2||numsSize==1)return -1;
-
-    for(int i=0;i<numsSize-1;i++){
-        for(int j=0;j<numsSize-1-i;j++){
-            if(nums[j]>nums[j+1]){
-              int temp =nums[j];
-              nums[j]=nums[j+1];
-              nums[j+1]=temp;
-            }
+    int max=INT_MIN,min=INT_MAX;
+    for(int i=0;i<numsSize;i++){
+       if(nums[i]>max)max=nums[i];
+       if(nums[i]<min)min=nums[i];
         }
+      for(int i=0;i<numsSize;i++){
+       if(nums[i]!=max&&nums[i]!=min)
+       return nums[i];
+       
+        }
+        return 0;
     }
-      return nums[numsSize/2];
-}
