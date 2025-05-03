568d96d2d730d029de235eb70e0ea22bf0b19524
keerthi <g.keerthiraghava37@gmail.com>
2025-05-03 06:50:36 +0530

    Time: 11 ms (78.26%), Space: 7.9 MB (84.06%) - LeetHub

--- 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.c
+++ 2566-number-of-unequal-triplets-in-array/2566-number-of-unequal-triplets-in-array.c
@@ -2,8 +2,10 @@ int unequalTriplets(int* a, int numsSize) {
     int sum = 0;
   for(int i=0;i<numsSize-2;i++){
     for(int j=i;j<numsSize-1;j++){
+        if(a[i]==a[j]){continue;}
         for(int k=j;k<numsSize;k++){
-            if(a[i]!=a[j]&&a[j]!=a[k]&&a[k]!=a[i])sum++;
+            if(a[j]==a[k]||a[k]==a[i]){continue;}
+            sum++;
         }
     }
   }
