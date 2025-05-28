d87e5a951f911560dd32051070e269e519b128dd
keerthi <g.keerthiraghava37@gmail.com>
2025-05-28 22:36:46 +0530

    Time: 0 ms (100%), Space: 10.9 MB (18.15%) - LeetHub

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int> res ; 
+        res.push_back(0);
+        for(int i=0;i<gain.size();i++){
+           res.push_back(res.back()+gain[i]);
+        }
+        return  *max_element(res.begin(),res.end());
+    }
+};
