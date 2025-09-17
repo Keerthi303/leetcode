7a0ae8a2cc9095b70dd160fd6971c42f53b16753
keerthi <g.keerthiraghava37@gmail.com>
2025-09-17 22:25:47 +0530

    Time: 0 ms (100%), Space: 11 MB (27.18%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     void subseq(int idx,vector<int> &a,vector<int> &ds,vector<vector<int>> &res,int n,map<vector<int>,int> &mpp){
        if(idx >= n){
-        vector<int> temp = ds;
-        sort(temp.begin(),temp.end());
-        if(mpp.find(temp) != mpp.end() &&mpp[temp] == 1) return;
-        res.push_back(temp);
-        mpp[temp]++;
+        if(mpp.find(ds) != mpp.end() &&mpp[ds] == 1) return;
+        res.push_back(ds);
+        mpp[ds]++;
         return ;
        }
        // pick
@@ -20,6 +18,7 @@ public:
         vector<int> ds;
         vector<vector<int>> res;
         map<vector<int>,int> mpp;
+        sort(nums.begin(),nums.end());
         subseq(0,nums,ds,res,nums.size(),mpp);
          return res;
     }
