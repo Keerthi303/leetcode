f06e814476a10016a93a82544321e5494437bcb8
keerthi <g.keerthiraghava37@gmail.com>
2026-07-18 11:14:24 +0530

    Time: 23 ms (98.97%), Space: 121.8 MB (61.86%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.cpp
@@ -5,16 +5,14 @@ public:
         vector<int> rows(n,0),col(m,0);
         for(int i = 0; i < n;i++){
             for(int j = 0; j < m ; j++){
-                if(grid[i][j] == 1){
-                    rows[i]++;
-                    col[j]++;
-                }
+                    rows[i]+=grid[i][j];
+                    col[j] +=grid[i][j];
             }
         }
         vector<vector<int>> ans(n,vector<int>(m,0));
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m ; j++){
-                ans[i][j] = rows[i]+col[j]-(n-rows[i])-(m-col[j]);
+                ans[i][j] = 2*(rows[i]+col[j])-n-m;
             }
         }
         return ans;
