92d2a8f70733d3fc633c4c594b89cffadec8f8fd
keerthi <g.keerthiraghava37@gmail.com>
2026-07-23 06:54:32 +0530

    Time: 28 ms (42.68%), Space: 149.1 MB (89.18%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -1,34 +1,20 @@
 class NumMatrix {
 public:
-    vector<vector<int>>prefix;
+    vector<vector<int>> prefix;
     NumMatrix(vector<vector<int>>& matrix) {
-        int n = matrix.size(),m = matrix[0].size();
-        prefix.resize(n,vector<int>(m,0));
-        for(int i = 0; i < n;i++){
-            prefix[i][0] = matrix[i][0];
-        }
-        for(int i = 0; i < n;i++){
-           for(int j = 1; j < m; j++){
-             prefix[i][j] = prefix[i][j-1]+matrix[i][j];
-           }
-        }
-    }
-    
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
-        int r1 = min(row1,row2),r2 = max(row1,row2);
-        int c1 = max(col1,col2),c2 = min(col1,col2);
-        for(int i = r1; i <= r2;i++){
-            if(c2 != 0){
-                sum += prefix[i][c1]-prefix[i][c2-1];
-            }
-            else {
-                sum += prefix[i][c1];
+        int n = matrix.size(), m = matrix[0].size();
+        prefix.resize(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                prefix[i+1][j+1] = prefix[i][j+1] + prefix[i+1][j] - prefix[i][j] + matrix[i][j];
             }
         }
-        return sum;
     }
 
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2+1][col2+1] - prefix[row1][col2+1] - prefix[row2+1][col1] + prefix[row1][col1];
+    }
 };
 
 /**
