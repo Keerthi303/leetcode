159c6a9ad680b074ab55bc84285a86f77ecd8a9a
keerthi <g.keerthiraghava37@gmail.com>
2026-08-26 15:44:03 +0530

    Time: 7 ms (77.3%), Space: 7.8 MB (90.81%) - LeetHub

--- 0866-prime-palindrome/0866-prime-palindrome.cpp
+++ 0866-prime-palindrome/0866-prime-palindrome.cpp
@@ -1,5 +1,5 @@
-class Solution {
 
+class Solution {
     bool is_prime(long long n){
         if(n == 0 || n == 1)  return false;
         for(long long i = 2; i*i <= n; i++){
@@ -8,18 +8,22 @@ class Solution {
         return true;
     }
     bool is_palindrome(int n){
-        string s = to_string(n);
-        string k = s;
-        reverse(s.begin(),s.end());        
-        return (s == k);
+       int k = n;
+       int l = 0;
+       while(n > 0){
+        int r = n%10;
+            l = (1LL*l*10)+r;
+            n/=10;
+       }
+       return (k == l);
     }
 public:
     int primePalindrome(int n) {
-        int k = n;
+      int k = n;
         if(n >=9989900) return 100030001;
         while(1){
-            if(is_prime(k)){
-                if(is_palindrome(k))
+            if(is_palindrome(k)){
+                if(is_prime(k))
                 return k;
             }
               k++;
